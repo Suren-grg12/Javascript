04bfab2a01cc217640e02b195a3ab70f6d34059c
Surendra Gurung <suren07@gmail.com>
2024-09-22 09:45:52 +0545

    first commit

---
+++ day1.js
@@ -0,0 +1,59 @@
+//console.log('hell0 world')
+//console.log('what i have done?')
+//console.log('is this working')
+//console.log('--watch is working')
+
+//var a = 'coffee';
+//a = 'water'   // var ma reassignment garna milcha 
+//console.log(a)
+
+
+//let a = 'coffee';
+//a = 'water'   // let  ma reassignment garna milcha 
+//console.log(a)
+
+
+//const
+//const a = ' watchout'
+//a = 'isee'
+// console.log(a)       //const ma re assignment garna mildai na 
+
+// if(true){
+//     // let name= 'john doe'
+//     // const name = 'john doe'
+//     var name ='john doe '
+
+//     console.log(name,'this is first one')
+// }
+// console.log(name,'this is second one')
+
+
+
+
+// let name = "surendra" 
+// console.log(name, "gurung")
+// let name = 10
+// console.log(name + '10')
+
+
+
+
+///////////////////////course array start////////////////////////////
+
+
+// let array = [ 'mero', 'tero' , 'usko' ]
+// console.log(array[0])
+// console.log(array[1])
+// console.log(array[2])
+// console.log(array)
+
+
+// let array = ['one', 1]
+// console.log(array[1])
+
+let array = {
+name : "surendra gurung" ,
+roll_number : 10
+}
+console.log(array.roll_number)
+console.log(array.name)
