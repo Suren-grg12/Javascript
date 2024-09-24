18b595ea319f67efc25bf87bf328e5f353291c5b
Surendra Gurung <suren07@gmail.com>
2024-09-24 23:04:31 +0545

    day 3 learning

---
+++ day3.js
@@ -0,0 +1,112 @@
+
+//--------------regular  function-----------------
+
+// function add(a,b) {         //add vaneko function //(a,b) paramater ho 
+//     console.log(a+b)
+// }
+// add(1,2)                   //argument vaneko pathaune kura lai value (1,2)
+
+
+
+
+// function add(a,b,c=10){                  //c lai defult banako
+//     console.log(a+b+c)
+// }
+// add(1,1,1)                      //argument ma pani and defult mai value deko cha vani --argument ko pirority dencha 
+
+
+
+
+/////---- named function-------
+
+// const add = function(a,b,c){
+//     console.log(a+b+c)
+// }
+// add(1,1,1)
+
+
+
+
+//-----or----one line code ----arrow  function----
+
+// const add = (a,b,c) => console.log(a+b+c)
+// add(1,2,3)
+
+ 
+//------------anonymous function -----------
+
+// const anonymous = ()=>{                               //name ma function assagin ko cha vani name arraw function huncha like anonymous vanko name vo =() le function assgain gareko
+//  console.log("i am anonymous")
+// }
+// anonymous()
+
+
+// const sqr = (a) =>{
+//     console.log(a*a)
+// }
+// sqr (5)
+
+ 
+
+
+
+
+//-----------------loops----------------------------------
+
+// let friends = ['manish','hari',"shyam"]
+// for (let i=0;i<friends.length;i++){
+//     console.log(friends[i])
+// }
+
+//----while loop ------ // rare case 
+// let friends = ['manish','hari',"shyam"]
+// let i = 0
+// while(i<friends.length){
+//     console.log(friends[i])
+//     i++
+// }
+
+
+//-------------easy way -------------------
+// let friends = ['manish','hari',"shyam"]
+// for (friends of friends){
+//     console.log(friends)
+// }
+
+
+// let friends = ['manish','hari',"shyam"]
+// for(hahah of friends){
+//     console.log(hahah)
+// }
+
+///// loop for object ////////
+
+// let person = {
+//     name :"amosh tamu" ,
+//     rollnumber : 12,
+//     address : "gorkha"
+// }
+// for(let  hahah in person){
+//     console.log(hahah,person[hahah])
+// }
+
+// console.log(name)
+// var name = "surendra"
+
+// add ()
+// var add = () => {
+//     console.log(1+1)
+// }
+
+
+
+// add()
+// var add = function(){
+//     console.log(1+1)
+// }
+
+function example(){
+console.log(y)
+var y=99
+}
+example()
